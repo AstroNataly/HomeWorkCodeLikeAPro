5157006f67f1b67d5b153124744de3dcf6fc7957
Natalya Podorozhnaya <dragonam1986@gmail.com>
2021-08-25 12:06:45 +0300

    Added translation, buttom, land

--- app/src/main/java/ru/netology/nmedia/MainActivity.kt
+++ app/src/main/java/ru/netology/nmedia/MainActivity.kt
@@ -7,5 +7,9 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        println("NMEDIA!")
+        println(R.string.nmedia)
+        println(getString(R.string.nmedia))
     }
 }

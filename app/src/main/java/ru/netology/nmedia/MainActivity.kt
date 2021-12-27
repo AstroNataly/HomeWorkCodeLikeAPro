97ef36ecfe5d308dedfc38782ce17a16adc27ab5
Natalya Podorozhnaya <dragonam1986@gmail.com>
2021-12-27 21:18:25 +0300

    Added likes

--- app/src/main/java/ru/netology/nmedia/MainActivity.kt
+++ app/src/main/java/ru/netology/nmedia/MainActivity.kt
@@ -2,11 +2,70 @@ package ru.netology.nmedia
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.ImageButton
+import android.widget.TextView
+import androidx.annotation.NonNull
+import ru.netology.nmedia.databinding.ActivityMainBinding
+import ru.netology.nmedia.dto.Post
+import kotlin.math.log
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        val binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
+        val post = Post(
+            id = 1,
+            author = "Нетология. Университет интернет-профессий будущего",
+            content = "Привет, это новая Нетология! Когда-то Нетология начиналась с интенсивов по онлайн-маркетингу. Затем появились курсы по дизайну, разработке, аналитике и управлению. Мы растём сами и помогаем расти студентам: от новичков до уверенных профессионалов. Но самое важное остаётся с нами: мы верим, что в каждом уже есть сила, которая заставляет хотеть больше, целиться выше, бежать быстрее. Наша миссия — помочь встать на путь роста и начать цепочку перемен → http://netolo.gy/fyb",
+            published = "21 мая в 18:36",
+
+
+
+        )
+        with(binding) {
+            author.text = post.author
+            content.text = post.content
+            published.text = post.published
+
+
+
+            setLike(post)
+
+            likesButton.setOnClickListener {
+                post.likedByMe = !post.likedByMe
+
+                setLike(post)
+                setCountLike(post)
+            }
+        }
     }
+
+    private fun  ActivityMainBinding.setLike(post: Post) {
+        likesButton.setImageResource(
+            if (post.likedByMe) {
+                R.drawable.ic_liked
+
+            } else {
+                R.drawable.ic_baseline_favorite_border_24
+
+            }
+        )
+    }
+
+    private fun  ActivityMainBinding.setCountLike(post: Post) {
+        countToLike.setText(
+            if (post.likedByMe) {
+                R.string.countLikesChanged
+
+            } else {
+                R.string.countLikes
+
+            }
+        )
+    }
+
+
 }
---
+++ app/src/main/java/ru/netology/nmedia/dto/Post.kt
@@ -0,0 +1,11 @@
+package ru.netology.nmedia.dto
+
+data class Post(
+    val id: Int = 0,
+    val author: String = "",
+    val content: String = "",
+    val published: String = "",
+    var likedByMe: Boolean = false,
+
+
+)
